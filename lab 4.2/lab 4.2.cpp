5fa2db109f3eb26e9eea851b3d2a9613506a7e3b
kyrakura <caidavid13@gmail.com>
2023-10-08 11:21:20 +0300

    lab 4.2 commit2

--- lab 4.2/lab 4.2.cpp
+++ lab 4.2/lab 4.2.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main()
 {
-	double x, xp, xk, dx, y{};
+	double x, xp, xk, dx, A, B{}, y{};
 	cout << "xp = "; cin >> xp;//початкове значення x
 	cout << "xk = "; cin >> xk;//кінцеве значення x
 	cout << "dx = "; cin >> dx;//крок зміни x(крок між значеннями x у діапазоні, для якого обчислюватиметься функція)
@@ -17,12 +17,14 @@ int main()
 	x = xp;
 	while (x <= xk)
 	{
+		A = pow(x, 2) * sin((4 * x) / 3);
 		if (x < -5)
-			y = 1 / tan(x);
+			B = 1 / tan(x);
 		else if (x >= -5 && x < 0)
-			y = pow(x, 2) * sin(4 * x / 3) + 4 * x / 2;
+			B = pow(x, 2) * sin(4 * x / 3) + 4 * x / 2;
 		else if (x > 0)
-			y = log10(pow(x, 2) - 4 * x / 3);
+			B = log10(pow(x, 2) - 4 * x / 3);
+		y = A + B;
 		//виводимо значення x та y у відповідному форматі
 		cout << "|" << setw(7) << setprecision(2) << x
 			<< " |" << setw(10) << setprecision(3) << y
